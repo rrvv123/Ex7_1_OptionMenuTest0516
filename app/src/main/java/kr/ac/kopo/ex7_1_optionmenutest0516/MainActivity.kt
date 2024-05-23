5e026ecfd1c5505ec1ea33e9739537abebebe10d
rrvv123 <eorms7391@gmail.com>
2024-05-23 14:23:37 +0900

    직접풀어보기 7-1 모두 수정완료 - 0523

--- app/src/main/java/kr/ac/kopo/ex7_1_optionmenutest0516/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/ex7_1_optionmenutest0516/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,14 +15,16 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit var linear : LinearLayout
-    lateinit var btn : Button
+    lateinit var editDegree : EditText
+    lateinit var imgv : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         linear = findViewById<LinearLayout>(R.id.linear)
-        btn = findViewById<Button>(R.id.btn)
+        editDegree = findViewById<EditText>(R.id.editDegree)
+        imgv = findViewById<ImageView>(R.id.imgv)
     }
 //    옵션메뉴 등록
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -34,25 +38,24 @@ class MainActivity : AppCompatActivity() {
 //    항목이 선택되었을 때 이벤트 처리할 수 있는 메소드
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.itemGray -> {
-                linear.setBackgroundColor(Color.GRAY)
-                return true
-            }
-            R.id.itemYellow -> {
-                linear.setBackgroundColor(Color.YELLOW)
-                return true
-            }
-            R.id.itemDkGray -> {
-                linear.setBackgroundColor(Color.DKGRAY)
-                return true
-            }
 
-            R.id.btnRotate -> {
-                btn.rotation += 60f // 실수형으로 표기해야됨. 따라서 float의 f를 붙여줌.
+            R.id.itemRotate -> {
+                imgv.rotation += editDegree.text.toString().toFloat()
                 return true
             }
-            R.id.btnZoomin -> {
-                btn.scaleX = 2f
+            R.id.item1 -> {
+                imgv.setImageResource(R.drawable.imgs1)
+                item.setChecked(true)
+                return true
+            }
+            R.id.item2 -> {
+                imgv.setImageResource(R.drawable.imgs2)
+                item.setChecked(true)
+                return true
+            }
+            R.id.item3 -> {
+                imgv.setImageResource(R.drawable.imgs3)
+                item.setChecked(true)
                 return true
             }
         }
